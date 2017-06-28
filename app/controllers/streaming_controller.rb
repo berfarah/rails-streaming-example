d9801cf377e1e358aaa6ffd8c57642926abacb73
Bernardo Farah <ber@bernardo.me>
2017-06-27 23:21:25 -0700

    Step 1

--- app/controllers/streaming_controller.rb
+++ app/controllers/streaming_controller.rb
@@ -1,5 +1,28 @@
+require 'csv'
+
 class StreamingController < ApplicationController
   def download
-    render html: ""
+    # Tell Rack to stream the content
+    headers.delete("Content-Length")
+
+    # Don't cache anything from this generated endpoint
+    headers["Cache-Control"] ||= "no-cache"
+
+    # Set an Enumerator as the body
+    self.response_body = body
+
+    # Set the status to success
+    response.status = 200
+  end
+
+  private
+
+  def body
+    Enumerator.new do |yielder|
+      20.times do |num|
+        sleep 0.05
+        yielder << CSV.generate_line([num, "yay"])
+      end
+    end
   end
 end
